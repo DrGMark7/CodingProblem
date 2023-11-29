e8be6c8f4603f09b7b7647578fa05b08e1cbbd16
DrGMark7 <72572485+DrGMark7@users.noreply.github.com>
2023-11-29 18:09:12 +0700

    Delete Filter Str.py

--- Filter Str.py
+++
@@ -1,6 +0,0 @@
-import string
-InWord = str(input('Enter text: '))
-OutWord = str(input('Enter filter word: '))
-InOut = len(OutWord)
-print('Text after filter is','"' +InWord.replace(OutWord,'*'*InOut) + '"')
-
