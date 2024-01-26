5be3ecce508e40dadae30d58318dee54604e2715
DrGMark7 <72572485+DrGMark7@users.noreply.github.com>
2024-01-26 11:05:59 +0700

    Delete fizzbuzzGit.py

--- fizzbuzzGit.py
+++
@@ -1,8 +0,0 @@
-n = int(input('Enter number: '))
-print('The number is',n)
-if n % 3 ==0 and n % 5 != 0 :
-    print('Fizz')
-if n % 3 !=0 and n % 5 == 0 :
-    print('Buzz')
-if n % 3 ==0 and n % 5 ==0 :
-    print('FizzBuzz')
