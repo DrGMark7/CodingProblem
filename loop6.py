39043621fdb4613784262d1a78e1ae0bf185d8ce
DrGMark7 <72572485+DrGMark7@users.noreply.github.com>
2024-02-13 21:49:01 +0700

    Delete loop6.py

--- loop6.py
+++
@@ -1,7 +0,0 @@
-num = int(input("Enter the number of rows: "))
-for i in range(num,0,-1):
-    for j in range(num-i,0,-1):
-        print(end=" ")
-    for j in range(i,0,-1):
-        print("*",end="")
-    print()
